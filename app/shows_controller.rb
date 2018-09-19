cced0644724c2b5874f9d2efef774adca9e26ff9
Koy Saeteurn <kosatun@aol.com>
2018-09-19 19:10:43 +0000

    automatically backed up by learn

--- app/shows_controller.rb
+++ app/shows_controller.rb
@@ -1,2 +1,23 @@
 def highest_rating 
+  Show.maximum(:rating)
+end   
+
+def most_popular_show 
+  shows.highest_rating
+end 
+
+def lowest_rating 
+  Show.minimum(:rating)
+end  
+
+def least_popular_show
+  shows.lowest_rating
+end   
+
+def ratings_sum 
+  Show.sum(:rating)
+end   
+
+def popular_shows 
   
+end   
